14f973e3281913e381258dbf5374daa769204ef7
muradctg <wri_murad@yahoo.com>
2022-05-12 12:25:03 -0400

    completed the register, login, logout routes, JWT, and refectored the code to work with cookies

--- app.js
+++ app.js
@@ -7,16 +7,24 @@ const app = express()
 
 const morgan = require('morgan')
 
+const cookieParser = require('cookie-parser')
+
 const connectDB = require('./db/connect')
+
+const authRouter = require('./routes/authRoutes')
+
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(morgan('tiny'))
 app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET))
 
 app.get('/', (req,res) => {
     res.send('ecommerce API')
 })
+
+app.use('/api/v1/auth', authRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
